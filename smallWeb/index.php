7e697fc0b429931418f163cb1b6cdc1760d87f7e
Tommmmmcat <61011490+Tommmmmcat@users.noreply.github.com>
2020-11-25 15:04:43 +0000

    Add files via upload

---
+++ smallWeb/index.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>gushi</title>
+        <link href="newstyle.scss" rel="stylesheet" type="text/css" />
+    </head> 
+    <body>
+        <main>
+         <h1 class="header">将进军</h1>
+        <p>（唐）李白</p>
+        <p>君不见，黄河之水天上来，奔流到海不复回。</p>
+        <p class="t">See you, the water of the Yellow River is coming up from the sky, rushing to the sea never to return. </p>
+
+        <p>君不见，高堂明镜悲白发，朝如青丝暮成雪。</p>
+        <p class="t">You don’t see me, Gaotangmingjing has white hair in sorrow, and the morning is like a green silk. </p>
+        <p>人生得意须尽欢，莫使金樽空对月。</p>
+        <p class="t">To be happy in life, don’t make the golden bottle empty against the moon. </p>
+        <p>天生我材必有用，千金散尽还复来。</p>
+        <p class="t">I am born to be useful, and I will come back when my wealth is gone. </p>
+        <p>烹羊宰牛且为乐，会须一饮三百杯。</p>
+        <p class="t">Cooking sheep and slaughtering cows is fun, and you will have to drink three hundred cups. </p>
+        <p>岑夫子，丹丘生，将进酒，杯莫停。</p>
+        <p class="t">Master Cen, Dan Qiusheng, will be drinking, not stop drinking. </p>
+        <p>与君歌一曲，请君为我倾耳听。</p>
+        <p class="t">A song with Junge, please listen to me. </p>
+        <p>钟鼓馔玉不足贵，但愿长醉不复醒。</p>
+        <p class="t">The bells, drums, and jade are not expensive, but I hope I won’t wake up forever. </p>
+        <p>古来圣贤皆寂寞，惟有饮者留其名。</p>
+        <p class="t">The sages and sages in ancient times are lonely, only the drinker keeps his name. </p>
+        <p>陈王昔时宴平乐，斗酒十千恣欢谑。</p>
+        <p class="t">In the old days, Wang Chen feasted and fought for peace and joy. </p>
+        <p>主人何为言少钱，径须沽取对君酌。</p>
+        <p class="t">What is the master's word for less money, you have to sell the right to the king. </p>
+        <p>五花马，千金裘，呼儿将出换美酒，与尔同销万古愁。</p>
+        <p class="t">Five-flowered horse, golden fur, Hu'er will exchange for fine wine, and sell the eternal sorrow with you. </p>
+
+            
+        </main>
+
+
+    </body>
+   
+
+</html>
